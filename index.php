e6f1161f95b2d84f7dffc11544194101b9a3bc0f
Flo <flosite@Flosites-MacBook-Pro.local>
2014-07-30 17:24:44 +0300

    New com 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<?php flo_part('featured-posts') ?>
+<?php flo_part('category-archive') ?>
+<?php flo_part('blog-header') ?>
+<div class="blog-wrapper">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<?php $current_post_id = get_the_ID(); ?>
+		<?php flo_part('posthead');?>
+		<?php flo_part('postpreview' );?>
+		<?php flo_part('postfull');?>
+		<?php flo_part('postfooter');?>
+	<?php endwhile; else: ?>
+		<?php flo_part('notfound')?>
+	<?php endif; ?>
+</div>
+<?php flo_page_links();?>
+<?php get_footer(); ?>
